91f600616c3d5ffd3ae1dd8e8308ecb83035ce38
Manish jha <jhamanish230@gmail.com>
2020-07-12 20:03:40 +0000

    update groovy script

--- vars/opsdeploy.groovy
+++ vars/opsdeploy.groovy
@@ -2,7 +2,7 @@ def buildNdeploy(branch,app_name,server_name,timestamp,role){
         dir('/home/ubuntu/ops_repo/bluegreen_deploy') {
         stage("Build for green ${role} & app name ${app_name}"){
             try{
-               sh "ansible-plpaybook main.yml -e 'host=${server_name} role=${role}'"
+               sh "ansible-playbook main.yml -e 'host=${server_name} role=${role}'"
             }
             catch(Exception e){
                 echo "build failed"
